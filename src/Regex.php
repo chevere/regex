80a7cac44fa520b1556cb23542f34459663eb79d
Rodolfo Berrios <20590102+rodber@users.noreply.github.com>
2024-01-08 09:18:35 -0300

    fix deps

--- src/Regex.php
+++ src/Regex.php
@@ -17,11 +17,7 @@ use Chevere\Regex\Exceptions\NoMatchException;
 use Chevere\Regex\Interfaces\RegexInterface;
 use InvalidArgumentException;
 use LogicException;
-use Safe\Exceptions\PcreException;
-use Throwable;
 use function Chevere\Message\message;
-use function Safe\preg_match;
-use function Safe\preg_match_all;
 
 final class Regex implements RegexInterface
 {
@@ -35,7 +31,7 @@ final class Regex implements RegexInterface
     public function __construct(
         private string $pattern
     ) {
-        $this->assertRegex();
+        $this->assertPattern();
         $delimiter = $this->pattern[0];
         $this->noDelimiters = trim($this->pattern, $delimiter);
         $this->noDelimitersNoAnchors = strval(
@@ -60,22 +56,19 @@ final class Regex implements RegexInterface
 
     public function match(string $value): array
     {
-        try {
-            $match = preg_match($this->pattern, $value, $matches);
+        $match = @preg_match($this->pattern, $value, $matches);
+        if (is_int($match)) {
+            return $match === 1 ? $matches : [];
         }
         // @codeCoverageIgnoreStart
-        catch (PcreException $e) {
-            throw new LogicException(
-                (string) message(
-                    'Error `%function%` %message%',
-                    function: 'preg_match',
-                    message: $e->getMessage()
-                )
-            );
-        }
+        throw new LogicException(
+            (string) message(
+                'Error `%function%` %error%',
+                function: 'preg_match',
+                error: static::ERRORS[preg_last_error()],
+            )
+        );
         // @codeCoverageIgnoreEnd
-
-        return $match === 1 ? $matches : [];
     }
 
     public function assertMatch(string $value): void
@@ -96,22 +89,19 @@ final class Regex implements RegexInterface
 
     public function matchAll(string $value): array
     {
-        try {
-            $match = preg_match_all($this->pattern, $value, $matches);
+        $match = @preg_match_all($this->pattern, $value, $matches);
+        if (is_int($match)) {
+            return $match === 1 ? $matches : [];
         }
         // @codeCoverageIgnoreStart
-        catch (PcreException $e) {
-            throw new LogicException(
-                (string) message(
-                    'Error `%function%` %message%',
-                    function: 'preg_match_all',
-                    message: $e->getMessage()
-                )
-            );
-        }
+        throw new LogicException(
+            (string) message(
+                'Error `%function%` %error%',
+                function: 'preg_match',
+                error: static::ERRORS[preg_last_error()],
+            )
+        );
         // @codeCoverageIgnoreEnd
-
-        return $match === 1 ? $matches : [];
     }
 
     public function assertMatchAll(string $value): void
@@ -130,19 +120,18 @@ final class Regex implements RegexInterface
         );
     }
 
-    private function assertRegex(): void
+    private function assertPattern(): void
     {
-        try {
-            preg_match($this->pattern, '');
-        } catch (Throwable $e) {
-            throw new InvalidArgumentException(
-                previous: $e,
-                message: (string) message(
-                    'Invalid regex string `%regex%` provided: %error%',
-                    regex: $this->pattern,
-                    error: static::ERRORS[preg_last_error()],
-                )
-            );
+        if (@preg_match($this->pattern, '') !== false) {
+            return;
         }
+
+        throw new InvalidArgumentException(
+            (string) message(
+                'Invalid regex pattern `%pattern%` provided: %error%',
+                pattern: $this->pattern,
+                error: static::ERRORS[preg_last_error()],
+            )
+        );
     }
 }
